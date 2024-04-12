2f2c25deb7d8c90f5c5cc12708333b9aea3a8d10
mmpotulo28 <mpotulom28@gmail.com>
2024-04-12 19:52:05 +0200

    Update CSS styles for homepage and quiz cards

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,43 @@
 const exploreBtn = document.getElementById('explore-btn');
 const landingOverlay = document.querySelector('.landing-overlay');
+const quizContainer = document.querySelector('.container');
 
 exploreBtn.addEventListener('click', () => {
 	landingOverlay.style.display = 'none';
 });
+
+const card = ({ id, name }) => {
+	return `
+	<div class="quiz-card" id="${id}">
+		<h1 class="heading">${name}</h1>
+		<button>Tap To Play!</button>
+	</div>
+`;
+};
+
+const quizTopics = [
+	{
+		id: 1,
+		name: 'Machine Learning',
+	},
+	{
+		id: 2,
+		name: 'Web Development',
+	},
+	{
+		id: 3,
+		name: 'Data Science',
+	},
+	{
+		id: 4,
+		name: 'Mobile App Development',
+	},
+];
+
+const renderCards = (data) => {
+	data.forEach((topic) => {
+		quizContainer.innerHTML += card(topic);
+	});
+};
+
+renderCards(quizTopics);
