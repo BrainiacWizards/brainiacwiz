754b2307acca9478303553b7c1d5546cfa811859
mmpotulo28 <mpotulom28@gmail.com>
2024-04-19 20:06:47 +0200

    add new contracts

--- migrations/2_deploy_quiz.js
+++
@@ -1,5 +0,0 @@
-const Quiz = artifacts.require('Quiz');
-
-module.exports = async function (deployer) {
-	await deployer.deploy(Quiz);
-};
---
+++ src/components/App.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Web3 from 'web3'
+import './App.css';
+import MemoryToken from '../abis/MemoryToken.json'
+import brain from '../brain.png'
+
+class App extends Component {
+
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '0x0'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          <a
+            className="navbar-brand col-sm-3 col-md-2 mr-0"
+            href="http://www.dappuniversity.com/bootcamp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          <img src={brain} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Memory Tokens
+          </a>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-muted"><span id="account">{this.state.account}</span></small>
+            </li>
+          </ul>
+        </nav>
+        <div className="container-fluid mt-5">
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+                <h1 className="d-4">Edit this file in App.js!</h1>
+
+                <div className="grid mb-4" >
+
+                  {/* Code goes here... */}
+
+                </div>
+
+                <div>
+
+                  {/* Code goes here... */}
+
+                  <div className="grid mb-4" >
+
+                    {/* Code goes here... */}
+
+                  </div>
+
+                </div>
+
+              </div>
+
+            </main>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
