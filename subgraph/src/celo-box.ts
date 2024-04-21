2bdd9f589fe521c788d25bb5d2bb179708d45146
mmpotulo28 <mpotulom28@gmail.com>
2024-04-21 17:46:36 +0200

    Refactor schema and CeloBox event handlers

--- subgraph/src/celo-box.ts
+++ subgraph/src/celo-box.ts
@@ -1,51 +1,26 @@
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  Transfer as TransferEvent
-} from "../generated/CeloBox/CeloBox"
-import { Approval, ApprovalForAll, Transfer } from "../generated/schema"
+	Approval as ApprovalEvent,
+	ApprovalForAll as ApprovalForAllEvent,
+	Transfer as TransferEvent,
+} from '../generated/CeloBox/CeloBox';
+import { Token, TokenMetaData, User } from '../generated/schema';
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
+const ipfshash = 'ipfs://';
 
 export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+	let token = Token.load(event.params.tokenId.toString());
+	if (!token) {
+		token = new Token(event.params.tokenId.toString());
+		token.tokenID = event.params.tokenId;
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+		token.tokenURI = '/' + event.params.tokenId.toString() + '.json';
+		const tokenIpsfsHash = ipfshash + token.tokenURI;
+		token.ipfsURI = tokenIpsfsHash;
 
-  entity.save()
+		TokenMetaDataTemplate.create(tokenIpsfsHash);
+	}
+
+	token.updatedAtTimestamp = event.block.timestamp;
+	token.owner = event.params.to;
+	token.save();
 }
