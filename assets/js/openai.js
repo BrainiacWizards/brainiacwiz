580f86a2db752956a414660afd95415681aee567
mmpotulo28 <mpotulom28@gmail.com>
2024-05-21 20:47:19 +0200

    chore: Add OpenAI token to environment variables

---
+++ assets/js/openai.js
@@ -0,0 +1,61 @@
+import { GoogleGenerativeAI } from 'https://cdn.skypack.dev/@google/generative-ai';
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const api_key = 'AIzaSyDgx9kucPZV4kAab55IzII0qFnxt2n26eY';
+const genAI = new GoogleGenerativeAI(api_key);
+
+async function run() {
+	// For text-only input, use the gemini-pro model
+	const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+	const prompt = `
+  1. Generate 6 multiple choice questions with 4 possible answers for the following topic:"Technology",
+  2. seperate the questions with a '--'.
+  3. each question should be preceded by a number (1), (2), (3), or (4).
+  4. each answer should be preceded by a letter (A), (B), (C), or (D).
+  5. the correct answer should be preceded by an equal sign (=) on a new line referencing to the correct answer.
+  `;
+
+	const result = await model.generateContent(prompt);
+	const response = result.response;
+	const text = response.text();
+	console.log(text);
+
+	// proccess response and add each question in a object with all the answers in an array
+	const questions = [];
+	const regex = {
+		question: /^\d/,
+		correctAnswer: /^=/,
+		answer: /^\([A-D]\)/,
+	};
+	const qs = text.split('--');
+
+	for (let q of qs) {
+		//loop through each question
+		let question = {
+			question: '',
+			answers: [],
+			correctAnswer: '',
+		};
+
+		for (let line of q.split('\n')) {
+			if (line.match(regex.question)) {
+				question.question = line;
+			}
+
+			if (line.match(regex.answer)) {
+				question.answers.push(line);
+			}
+
+			if (line.match(regex.correctAnswer)) {
+				question.correctAnswer = line.replace('=', '').trim();
+			}
+		}
+
+		questions.push(question);
+	}
+
+	console.log(questions);
+}
+
+run();
