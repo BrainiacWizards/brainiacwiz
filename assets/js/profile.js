e1039b032ebd066b6a89e20fc6afe697dc31f4d6
Manelisi Mpotulo <mpotulom28@gmail.com>
2024-06-26 01:16:58 +0200

    chore: Add "setnavbar" to list of supported names in settings.json

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -6,12 +6,7 @@ const txContent = document.querySelector('.tx-content');
 const profileTxs = document.getElementById('profile-tx-btn');
 const profileNfts = document.getElementById('profile-nft-btn');
 
-console.log(walletContainer);
-console.log(walletContent);
-console.log(txContent);
-
 profileNfts.addEventListener('click', () => {
-	console.log('profile nfts');
 	walletContainer.style.display = 'flex';
 	walletContent.style.display = 'flex';
 	txContent.style.display = 'none';
@@ -24,6 +19,4 @@ profileTxs.addEventListener('click', () => {
 	walletContent.style.display = 'none';
 	txContent.style.display = 'flex';
 	navbar.showTransfers();
-	console.log(walletContent.style.display);
-	console.log(txContent.style.display);
 });
