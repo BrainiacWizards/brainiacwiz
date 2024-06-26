aa41cc3b397025b1702f8ff98451c3ee110f9c5a
Manelisi Mpotulo <mpotulom28@gmail.com>
2024-06-26 02:50:04 +0200

    Refactor CSS and JavaScript for profile page

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -7,12 +7,7 @@ const txContent = document.querySelector('.tx-content');
 const profileTxs = document.getElementById('profile-tx-btn');
 const profileNfts = document.getElementById('profile-nft-btn');
 
-console.log(walletContainer);
-console.log(walletContent);
-console.log(txContent);
-
 profileNfts.addEventListener('click', () => {
-	console.log('profile nfts');
 	walletContainer.style.display = 'flex';
 	walletContent.style.display = 'flex';
 	txContent.style.display = 'none';
@@ -20,13 +15,10 @@ profileNfts.addEventListener('click', () => {
 });
 
 profileTxs.addEventListener('click', () => {
-	console.log('profile tx');
 	walletContainer.style.display = 'flex';
 	walletContent.style.display = 'none';
 	txContent.style.display = 'flex';
 	navbar.showTransfers();
-	console.log(walletContent.style.display);
-	console.log(txContent.style.display);
 });
 
 // Function to get and display the user's MetaMask address and balance
@@ -54,11 +46,9 @@ async function getAddress() {
 
 function updateBalanceElement(balance) {
 	const balanceElement = document.getElementById('profile-balance');
-	const balanceInDecimal = (balance / 10 ** 18).toFixed(8);
-	balanceElement.value = balanceInDecimal;
-	console.log('User balance:', balanceElement.value);
+	const balanceInDecimal = (balance / 10 ** 18).toFixed(2);
+	balanceElement.textContent = balanceInDecimal;
 }
 
 // Call getAddress to display balance
 getAddress();
-
